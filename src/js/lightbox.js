43d4ca0b9652144a1d0c2c89abb4229503e64dcf
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-08-18 20:34:47 +0200

    js11

--- src/js/lightbox.js
+++
@@ -1,9 +0,0 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-export let lightbox = new SimpleLightbox('.img_wrap a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,59 @@
 import axios from 'axios';
 
-const URL = 'https://pixabay.com/api/';
-const KEY = '37440122-e5d5a2493910548fa520b3add';
+const API_KEY = '29796536-5ed99ce8effe96d6d69c656a8';
+const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchPhoto(q, page, perPage) {
-  const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const response = await axios.get(url);
-  return response.data;
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.loadedHits = 0;
+  }
+
+  async fetchImages() {
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: 40,
+    });
+
+    const url = `${BASE_URL}?${searchParams}`;
+
+    try {
+      const response = await axios.get(url);
+      this.incrementPage();
+
+      return response.data;
+    } catch (error) {
+      console.warn(`${error}`);
+    }
+  }
+
+  incrementLoadedHits(hits) {
+    this.loadedHits += hits.length;
+  }
+
+  resetLoadedHits() {
+    this.loadedHits = 0;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
